dbdc1e08c919b4b7d98b2248d05407db2a62bec2
Luciano Sabino <lb255043@gmail.com>
2025-04-12 11:59:25 -0300

    prisma

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import { app } from "./server/server.js";
+import { execSync } from "child_process";
 
+// Rodando o comando prisma generate
+try {
+  execSync("npx prisma generate", { stdio: "inherit" });
+  console.log("Prisma Client gerado com sucesso!");
+} catch (error) {
+  console.error("Erro ao gerar Prisma Client:", error);
+  process.exit(1); // Se falhar, encerra a aplicação com erro
+}
+
+// Inicia o servidor após gerar o Prisma Client
 app.listen(process.env.PORT, () => {
-  console.log(`Api rodando na porta 8080`);
+  console.log(`Api rodando na porta ${process.env.PORT || 8080}`);
 });
