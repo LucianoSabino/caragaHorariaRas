d4a6344299abcee60a68eeea7c77f3426001dc59
Luciano Sabino <lb255043@gmail.com>
2025-04-12 21:20:28 -0300

    mais configuração

--- src/server/database/prisma.js
+++ src/server/database/prisma.js
@@ -1,5 +1,22 @@
-// src/server/database/prisma.js
+// // src/server/database/prisma.js
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+// export default prisma;
+
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+let prisma;
+
+if (process.env.NODE_ENV === "production") {
+  // Em produção, crie uma única instância do PrismaClient
+  prisma = new PrismaClient();
+} else {
+  // Em desenvolvimento, use uma instância global para evitar recriações
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
 export default prisma;
